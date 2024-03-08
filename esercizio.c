4cde25d17810726dae3c2f40ed776927295fe29e
Mattiaaaa72 <161334921+Mattiaaaa72@users.noreply.github.com>
2024-03-08 10:28:34 +0100

    Create esercizio.c

---
+++ esercizio.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ int main() 
+{    
+int numero1, 
+int prodotto;
+      printf("Calcoliamo la moltiplicazione tra due numeri.\n");
+      printf("Inserisci il primo numero: ");   
+      scanf("%d", &numero1); 
+      printf("Inserisci il secondo numero: "); 
+      scanf("%d", &numero2); 
+      prodotto = numero1 * numero2;
+      printf("Il prodotto dei due numeri è: %d\n", prodotto); 
+return 0;
+}
+
